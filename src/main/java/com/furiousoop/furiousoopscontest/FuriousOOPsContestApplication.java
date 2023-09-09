1730ae403314af2e36cdb9127d612e288b357852
Alex <leonovalexprog@gmail.com>
2023-09-09 14:57:27 +0300

    upd: create baseClient template

---
+++ src/main/java/com/furiousoop/furiousoopscontest/FuriousOOPsContestApplication.java
@@ -0,0 +1,13 @@
+package com.furiousoop.furiousoopscontest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuriousOOPsContestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FuriousOOPsContestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/furiousoop/furiousoopscontest/FuriousOoPsContestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.furiousoop.furiousoopscontest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuriousOoPsContestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
