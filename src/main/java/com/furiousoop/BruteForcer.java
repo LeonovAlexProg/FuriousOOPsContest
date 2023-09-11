49c5bf89d09d5da7113d98f794a57a01fd57aa6c
SergSeYakovlev <serg.s.yakovlev@gmail.com>
2023-09-11 22:53:39 +0300

    Task3

---
+++ src/main/java/com/furiousoop/BruteForcer.java
@@ -0,0 +1,33 @@
+package com.furiousoop;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BruteForcer {
+    String alphabet = "0123456789ABCDEFabcdefZ";
+    public List<String> passwords(){
+        List<String> results = new ArrayList<>();
+        for(String symbol1: alphabet.split("")) {
+            for(String symbol2: alphabet.split("")) {
+                for(String symbol3: alphabet.split("")) {
+                    for(String symbol4: alphabet.split("")) {
+                        for(String symbol5: alphabet.split("")) {
+                            for(String symbol6: alphabet.split("")) {
+                                for(String symbol7: alphabet.split("")) {
+                                    for(String symbol8: alphabet.split("")) {
+                                        String result = symbol1 + symbol2 + symbol3 + symbol4 + symbol5 + symbol6 + symbol7 + symbol8;
+                                    results.add(result.replace("Z", ""));
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+        }
+        return results;
+    }
+
+}
+
